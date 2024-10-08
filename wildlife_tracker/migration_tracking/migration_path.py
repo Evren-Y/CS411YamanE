bdbb0e4887e10e194bb8ee80433ea5af0940f2ea
Evren Yaman <yamane@bu.edu>
2024-10-08 03:24:49 -0400

    Requirements and Design HW

---
+++ wildlife_tracker/migration_tracking/migration_path.py
@@ -0,0 +1,24 @@
+from typing import Optional
+
+from wildlife_tracker.migration_tracking.migration import Migration
+from wildlife_tracker.habitat_management.habitat import Habitat
+
+class MigrationPath:
+
+    def __init__(self,
+                destination: Habitat,
+                path_id: int,
+                species: str,
+                start_location: Habitat,
+                duration: Optional[int] = None) -> None:
+        self.destination = destination
+        self.path_id = path_id
+        self.species = species
+        self.start_lcoation = start_location
+        self.duration = duration
+
+    def get_migration_path_details(path_id) -> dict:
+        pass
+
+    def update_migration_path_details(path_id: int, **kwargs) -> None:
+        pass
